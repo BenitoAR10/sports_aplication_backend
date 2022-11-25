467beb3d790f5e56ffbbe3b8d5a8f92f07ec1e12
unknown <https://github.com/BenitoAR10?tab=repositories>
2022-11-25 17:58:43 -0400

    cambiando configuracion BBDD

--- bbdd/init.sql
+++ bbdd/init.sql
@@ -1,26 +1,5 @@
 -- Datos de prueba
 
--- INSERTS PERSONA
-insert into sp_persona (nombres, apellidos, genero, numero_telefono, ciudad, fecha_nacimiento, altura, peso, indice_masa_corporal, foto, cat_persona_status, estado, tx_correo, tx_fecha, tx_host)
-values ('Jamison', 'Filan', false, '916-795-5076', 'Sacramento', '2022-07-27', 162, 148, 36, 'http://dummyimage.com/171x100.png/5fa2dd/ffffff', true, false, 'jfilan0@nationalgeographic.com', '2022-03-24', '152.218.17.125');
-
-INSERT INTO sp_persona (nombres, apellidos, genero, numero_telefono, ciudad, fecha_nacimiento, altura, peso, indice_masa_corporal, foto, cat_persona_status, estado, tx_correo, tx_fecha, tx_host)
-VALUES ('Amara', 'Lampaert', false, '994-560-9729', 'Binzhou', '2022-09-08', 204, 67, 24, 'http://dummyimage.com/155x100.png/5fa2dd/ffffff', true, true, 'alampaert1@businessweek.com', now(), '167.130.231.107');
-
-INSERT INTO sp_persona (nombres, apellidos, genero, numero_telefono, ciudad, fecha_nacimiento, altura, peso, indice_masa_corporal, foto, cat_persona_status, estado, tx_correo, tx_fecha, tx_host)
-VALUES ('Mario', 'Clements', false, '580-708-5752', 'Vilares', '2022-06-20', 207, 41, 29, 'http://dummyimage.com/133x100.png/ff4444/ffffff', false, true, 'mclementsb@flickr.com', now(), '142.158.189.231');
-
-INSERT INTO sp_persona (nombres, apellidos, genero, numero_telefono, ciudad, fecha_nacimiento, altura, peso, indice_masa_corporal, foto, cat_persona_status, estado, tx_correo, tx_fecha, tx_host)
-VALUES ('Carlitos', 'Sucha', false, '580-708-5752', 'Vilares', '2022-06-20', 207, 41, 29, 'http://dummyimage.com/133x100.png/ff4444/ffffff', false, true, 'carlitos@gmail.com', now(), '142.158.189.231');
-
-INSERT INTO sp_persona (nombres, apellidos, genero, numero_telefono, ciudad, fecha_nacimiento, altura, peso, indice_masa_corporal, foto, cat_persona_status, estado, tx_correo, tx_fecha, tx_host)
-VALUES ('Camila', 'Sucha', false, '580-708-5752', 'Vilares', '2022-06-20', 207, 41, 29, 'http://dummyimage.com/133x100.png/ff4444/ffffff', false, true, 'carlitos@gmail.com', now(), '142.158.189.231');
-
-INSERT INTO sp_persona (nombres, apellidos, genero, numero_telefono, ciudad, fecha_nacimiento, altura, peso, indice_masa_corporal, foto, cat_persona_status, estado, tx_correo, tx_fecha, tx_host)
-VALUES ('Camila', 'Sucha', false, '580-708-5752', 'Vilares', '2022-06-20', 207, 41, 29, 'http://dummyimage.com/133x100.png/ff4444/ffffff', false, true, 'anonymus', now(), '142.158.189.231');
-
-SELECT * FROM sp_persona;
-
 -- INSERTS DEPORTE
 insert into sp_deporte (deporte, estado, tx_correo, tx_fecha, tx_host) values ('Atletismo', true, 'alampaert1@businessweek.com', now(), '167.130.231.107');
 insert into sp_deporte (deporte, estado, tx_correo, tx_fecha, tx_host) values ('Futbol', true, 'alampaert1@businessweek.com', now(), '167.130.231.107');
@@ -168,19 +147,7 @@ ALTER TABLE sp_cuenta ALTER COLUMN tx_fecha DROP NOT NULL;
 ALTER TABLE sp_cuenta ALTER COLUMN tx_host DROP NOT NULL;
 
 
--- Cambiando campos a nulleables en la tabla sp_persona.
-ALTER TABLE sp_persona ALTER COLUMN genero DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN cat_persona_status DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN ciudad DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN fecha_nacimiento DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN altura DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN peso DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN indice_masa_corporal DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN foto DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN estado DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN tx_correo DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN tx_fecha DROP NOT NULL;
-ALTER TABLE sp_persona ALTER COLUMN tx_host DROP NOT NULL;
+
 
 -- Cambiando campos a nulleables en la tabla sp_tienda.
 ALTER TABLE sp_tienda ALTER COLUMN latitud DROP NOT NULL;
@@ -190,8 +157,7 @@ ALTER TABLE sp_tienda ALTER COLUMN tx_correo DROP NOT NULL;
 ALTER TABLE sp_tienda ALTER COLUMN tx_fecha DROP NOT NULL;
 ALTER TABLE sp_tienda ALTER COLUMN tx_host DROP NOT NULL;
 
--- elinar trigger y procedimiento.
-DROP TRIGGER inst_cuenta ON sp_persona;
+
 
 
 --------------SP_TIENDA----------------
