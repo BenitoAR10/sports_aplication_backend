88c490427b52116636321dfe498108d07fdebde0
unknown <https://github.com/BenitoAR10?tab=repositories>
2022-11-25 23:47:50 -0400

    Creacion de cuenta con rol de servicio comida exitosa

---
+++ src/main/java/bo/edu/ucb/spapp/Sports/App/bl/CuentaServicioComidaBl.java
@@ -0,0 +1,41 @@
+package bo.edu.ucb.spapp.Sports.App.bl;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import bo.edu.ucb.spapp.Sports.App.dao.CuentaServicioComidaDao;
+import bo.edu.ucb.spapp.Sports.App.dao.SpCuentaDao;
+import bo.edu.ucb.spapp.Sports.App.dto.CrearCuentaDto;
+import bo.edu.ucb.spapp.Sports.App.dto.CrearServicioComidaDto;
+import bo.edu.ucb.spapp.Sports.App.entity.SpCuenta;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CuentaServicioComidaBl {
+
+    private SpCuentaDao spCuentaDao;
+
+    private CuentaServicioComidaDao cuentaServicioComidaDao;
+
+    public CuentaServicioComidaBl(SpCuentaDao spCuentaDao, CuentaServicioComidaDao cuentaServicioComidaDao) {
+        this.spCuentaDao = spCuentaDao;
+        this.cuentaServicioComidaDao = cuentaServicioComidaDao;
+    }
+
+    public void crearCuentaServicioComida(CrearCuentaDto crearCuentaDto){
+        SpCuenta spCuenta = new SpCuenta();
+        spCuenta.setIdDeporte(crearCuentaDto.getIdDeporte());
+        spCuenta.setCorreo(crearCuentaDto.getCorreo());
+        spCuenta.setNombres(crearCuentaDto.getNombre());
+        spCuenta.setApellidos(crearCuentaDto.getApellido());
+        spCuenta.setNumeroTelefono(crearCuentaDto.getNumeroTelefono());
+        String contrasenia = BCrypt.withDefaults().hashToString(12, crearCuentaDto.getContrasenia().toCharArray());
+        crearCuentaDto.getContrasenia();
+        spCuenta.setContrasena(contrasenia);
+        this.spCuentaDao.crearCuenta(spCuenta);
+
+        Integer idCuenta = spCuentaDao.findIdByCorreo(crearCuentaDto.getCorreo());
+        this.cuentaServicioComidaDao.cuentaGrupoServicioComida(idCuenta);
+
+
+    }
+
+}
